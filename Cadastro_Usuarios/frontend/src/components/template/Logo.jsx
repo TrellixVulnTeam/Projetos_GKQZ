3f41a761a537984e00a60cf763e29fb3c849ba0f
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2022-07-11 21:41:33 -0300

    Envio

---
+++ Cadastro_Usuarios/frontend/src/components/template/Logo.jsx
@@ -0,0 +1,14 @@
+import './Logo.css'
+import React from 'react'
+import logo from '../../assets/imgs/logo.png'
+import { Link } from 'react-router-dom'
+
+export default function Logo(props) {
+  return (
+    <aside className="logo">
+      <Link to={"/"} className="logo">
+        <img src={logo} alt="logo" />
+      </Link>
+    </aside>
+  )
+}
---
+++ Cadastro_Usuarios/frontend/src/main/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './Routes'
+
+import Logo from '../components/template/Logo'
+import Nav from '../components/template/Nav'
+import Header from '../components/template/Header'
+import Footer from '../components/template/Footer'
+
+export default function App(props) {
+  return (
+    <BrowserRouter>
+      <div className='app'>
+        <Logo />
+        <Header></Header>
+        <Nav></Nav>
+        <Routes/>
+        <Footer></Footer>
+      </div>
+    </BrowserRouter>
+  )
+}
+
