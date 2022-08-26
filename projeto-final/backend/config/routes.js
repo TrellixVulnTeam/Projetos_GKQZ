bf09db0218a08ccf16fbb7b18f0f5b10dd1b68aa
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2022-08-25 21:38:03 -0300

    update

--- projeto-final/backend/config/routes.js
+++ projeto-final/backend/config/routes.js
@@ -1,3 +1,10 @@
 module.exports = app => {
-    app.route("/users").post(app.api.user.save)
+    app.route("/users").post(app.api.user.save).get(app.api.user.get)
+
+    app.route("/users/:id").put(app.api.user.save).get(app.api.user.getByID)
+
+    app.route("/categories").get(app.api.categories.get).post(app.api.categories.save)
+
+    app.route("/categories/:id").get(app.api.categories.getById).put(app.api.categories.save).delete(app.api.categories.remove)
+
 }
--- projeto-final/backend/index.js
+++ projeto-final/backend/index.js
@@ -4,7 +4,7 @@ const db = require("./config/db")
 
 app.db = db
 
-consign().then("./config/middlewares.js").then("./api").then("./config/routes.js").into(app)
+consign().then("./config/middlewares.js").then("./api/validation.js").then("./api").then("./config/routes.js").into(app)
 
 app.listen(3000, () => {
     console.log("Executando backend na porta 3000.")
