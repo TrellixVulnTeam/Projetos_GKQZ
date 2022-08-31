31adfca1d5dafd2a7e2570b20c0b4c34e985b499
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2022-08-30 21:30:23 -0300

    up

--- projeto-final/backend/api/user.js
+++ projeto-final/backend/api/user.js
@@ -13,6 +13,9 @@ module.exports = app => {
         const user = { ...req.body }
         if (req.params.id) user.id = req.params.id
 
+        if (!req.originalUrl.startsWith("/users")) user.admin = false
+        if (!req.user || !req.user.admin) user.admin = false
+
         try {
             existsOrError(user.name, "Nome não cadastrado.")
             existsOrError(user.email, "E-mail não cadastrado.")
@@ -32,19 +35,33 @@ module.exports = app => {
         delete user.confirmPassword
 
         if (user.id) {
-            app.db("users").update(user).where({ id: user.id }).then(_ => res.status(204).send()).catch(err => res.status(500).send(err))
+            app.db("users").update(user).where({ id: user.id }).whereNull("deletedAt").then(_ => res.status(204).send()).catch(err => res.status(500).send(err))
         } else {
             app.db("users").insert(user).then(_ => res.status(204).send()).catch(err => res.status(500).send(err))
         }
     }
 
     const get = (req, res) => {
-        app.db("users").select("id", "name", "email", "admin").then(users => res.json(users)).catch(err => res.status(500).send(err))
-    }
-    
-    const getByID = (req, res) => {
-        app.db("users").select("id", "name", "email", "admin").where({ id: req.params.id }).then(user => res.json(user)).catch(err => res.status(500).send(err))
+        app.db("users").select("id", "name", "email", "admin").whereNull("deletedAt").then(users => res.json(users)).catch(err => res.status(500).send(err))
     }
 
-    return { save, get, getByID }
+    const getByID = (req, res) => {
+        app.db("users").select("id", "name", "email", "admin").where({ id: req.params.id }).whereNull("deletedAt").then(user => res.json(user)).catch(err => res.status(500).send(err))
+    }
+
+    const remove = async (req, res) => {
+        try {
+            const articles = await app.db("articles").where({ userId: req.params.id })
+            notExistsOrError(articles, "Usuário possui artigos cadastrados.")
+
+            const rowsUpdated = await app.db("users").update({ deletedAt: new Date() }).where({ id: req.params.id })
+            existsOrError(rowsUpdated, "Usuário não foi encontrado.")
+            res.status(204).send()
+
+        } catch (msg) {
+            res.status(400).send(msg)
+        }
+    }
+
+    return { save, get, getByID, remove }
 }
--- projeto-final/backend/index.js
+++ projeto-final/backend/index.js
@@ -7,7 +7,7 @@ require("./config/mongodb")
 app.db = db
 app.mongoose = mongoose
 
-consign().include("./config/passport.js").then("./config/middlewares.js").then("./api/validation.js").then("./api").then("./config/routes.js").into(app)
+consign().include("./config/passport.js").then("./config/middlewares.js").then("./api/validation.js").then("./api").then("./schedule").then("./config/routes.js").into(app)
 
 app.listen(3000, () => {
     console.log("Executando backend na porta 3000.")
